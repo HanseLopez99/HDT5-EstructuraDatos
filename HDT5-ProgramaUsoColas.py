174b0a3c8d7a544ed30eef11b503a279f70bd331
HanseLopez99 <halm29799@gmail.com>
2020-03-04 23:27:38 -0600

    Ligeros Cambios

--- HDT5-ProgramaUsoColas.py
+++ HDT5-ProgramaUsoColas.py
@@ -9,7 +9,7 @@ def main():
     env = simpy.Environment()
     RANDOM_SEED = 40
     random.seed(RANDOM_SEED)
-    processor = simpy.Resource(env, capacity=3)
+    processor = simpy.Resource(env, capacity=1)
     RAM = simpy.Container(env, init=100, capacity=100)
     totalProcesses = 25
     creationInterval = 10.0
@@ -22,7 +22,7 @@ def source(env, number, interval, processor, ram):
     for n in range(number):
         memoria = random.randint(1,10)  
         ram.get(memoria)
-        proc = processorSimulation(env, 'Process%02d' % n, processor, processorCapacity=3.0, ram=memoria)
+        proc = processorSimulation(env, 'Process%02d status' % n, processor, processorCapacity=1.0, ram=memoria)
         env.process(proc)
         time = random.expovariate(1.0 / interval)
         yield env.timeout(time)   
@@ -34,14 +34,13 @@ def processorSimulation(env, name, processor, processorCapacity, ram):
 
     with processor.request() as req:
         patience = random.uniform(1, 3)
-        # Wait for the counter or abort at the end of our tether
         results = yield req | env.timeout(patience)
 
         wait = env.now - arrive
 
         if req in results:
             # We got to the counter
-            print('%7.2f %s: Waited %6.2f' % (env.now, name, wait))
+            print('%7.2f %s: Waited  %6.2f' % (env.now, name, wait))
 
             tib = random.expovariate(1.0 / processorCapacity)
             yield env.timeout(tib)
