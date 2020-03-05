a4c08c7c544deff81398705e43ee2832edafbf48
HanseLopez99 <halm29799@gmail.com>
2020-03-05 00:20:06 -0600

    Ligeros Cambios

--- HDT5-ProgramaUsoColas.py
+++ HDT5-ProgramaUsoColas.py
@@ -9,21 +9,23 @@ def main():
     env = simpy.Environment()
     RANDOM_SEED = 40
     random.seed(RANDOM_SEED)
-    processor = simpy.Resource(env, capacity=1)
+    processor = simpy.Resource(env, capacity=2)
     RAM = simpy.Container(env, init=100, capacity=100)
     totalProcesses = 25
     creationInterval = 10.0
-    memoria = 0 
+    memory = 0 
+    numberOfInstructions = 0
     env.process(source(env, totalProcesses, creationInterval, processor, RAM))
     env.run()
     print("-Simulation Complete!")
     
 def source(env, number, interval, processor, ram):
     for n in range(number):
-        memoria = random.randint(1,10)  
-        ram.get(memoria)
+        memory = random.randint(1,10)  
+        numberOfInstructions = random.randint(1,10)
+        ram.get(memory)
         proc = processorSimulation(env, 'Process%02d status' % n, processor, processorCapacity=1.0)
-        ram.put(memoria)
+        ram.put(memory)
         env.process(proc)
         time = random.expovariate(1.0 / interval)
         yield env.timeout(time)   
@@ -41,15 +43,11 @@ def processorSimulation(env, name, processor, processorCapacity):
 
         if req in results:
             print('%7.2f %s: Waited  %6.2f' % (env.now, name, wait))
-
             time = random.expovariate(1.0 / processorCapacity)
             yield env.timeout(time)
-            print('%7.2f %s: Finished' % (env.now, name))
-            
+            print('%7.2f %s: Finished' % (env.now, name))           
         else:
             print('%7.4f %s: RENEGED after %6.2f' % (env.now, name, wait))
-
-        
 
 if __name__ == '__main__':
     main()
